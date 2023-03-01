35ec8fc3e49428bad0d63d68486400b34d498e04
guillaume <guillaume.belanger27@gmail.com>
2023-03-01 10:44:53 -0500

    Adds support for relation to DB

--- src/charm.py
+++ src/charm.py
@@ -7,8 +7,9 @@
 import logging
 from ipaddress import IPv4Address
 from subprocess import check_output
-from typing import Optional, Union
+from typing import Dict, Optional, Union
 
+from charms.data_platform_libs.v0.data_interfaces import DatabaseRequires
 from charms.nrf_operator.v0.nrf import NRFAvailableEvent, NRFRequires
 from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
 from jinja2 import Environment, FileSystemLoader
@@ -22,6 +23,7 @@ logger = logging.getLogger(__name__)
 
 BASE_CONFIG_PATH = "/etc/udr"
 CONFIG_FILE_NAME = "udrcfg.conf"
+DATABASE_NAME = "free5gc"
 
 
 class UDROperatorCharm(CharmBase):
@@ -31,11 +33,15 @@ class UDROperatorCharm(CharmBase):
         super().__init__(*args)
         self._container_name = self._service_name = "udr"
         self._container = self.unit.get_container(self._container_name)
+        self._database = DatabaseRequires(
+            self, relation_name="database", database_name=DATABASE_NAME, extra_user_roles="admin"
+        )
         self._nrf_requires = NRFRequires(charm=self, relationship_name="nrf")
         self.framework.observe(self.on.udr_pebble_ready, self._on_udr_pebble_ready)
         self.framework.observe(self.on.nrf_relation_created, self._on_udr_pebble_ready)
-        self.framework.observe(self._nrf_requires.on.nrf_available, self._on_nrf_available)
-
+        self.framework.observe(self._nrf_requires.on.nrf_available, self._on_udr_pebble_ready)
+        self.framework.observe(self.on.database_relation_joined, self._on_udr_pebble_ready)
+        self.framework.observe(self._database.on.database_created, self._on_udr_pebble_ready)
         self._service_patcher = KubernetesServicePatch(
             charm=self,
             ports=[
@@ -43,18 +49,12 @@ class UDROperatorCharm(CharmBase):
             ],
         )
 
-    def _on_nrf_available(self, event: NRFAvailableEvent) -> None:
-        if not self._container.can_connect():
-            self.unit.status = WaitingStatus("Waiting for container to be ready")
-            event.defer()
-            return
-        self._write_config_file(nrf_url=event.url)
-        self._on_udr_pebble_ready(event)
-
-    def _write_config_file(self, nrf_url: str) -> None:
+    def _write_config_file(self, nrf_url: str, database_url: str) -> None:
         jinja2_environment = Environment(loader=FileSystemLoader("src/templates/"))
         template = jinja2_environment.get_template("udrcfg.conf.j2")
         content = template.render(
+            database_name=DATABASE_NAME,
+            database_url=database_url,
             nrf_url=nrf_url,
             udr_hostname=self._udr_hostname,
         )
@@ -81,19 +81,54 @@ class UDROperatorCharm(CharmBase):
         return True
 
     def _on_udr_pebble_ready(self, event: Union[PebbleReadyEvent, NRFAvailableEvent]) -> None:
+        if not self._database_relation_is_created:
+            self.unit.status = BlockedStatus("Waiting for database relation to be created")
+            return
         if not self._nrf_relation_is_created:
             self.unit.status = BlockedStatus("Waiting for NRF relation to be created")
+            return
+        if not self._database_is_available:
+            self.unit.status = WaitingStatus("Waiting for database to be ready")
+            return
+        if not self._nrf_data_is_available:
+            self.unit.status = WaitingStatus("Waiting for NRF data to be available")
             return
         if not self._container.can_connect():
             self.unit.status = WaitingStatus("Waiting for container to be ready")
             event.defer()
             return
         if not self._config_file_is_written:
-            self.unit.status = WaitingStatus("Waiting for config file to be written")
-            return
+            self._write_config_file(
+                nrf_url=self._nrf_requires.get_nrf_url(),
+                database_url=self._database_data["uris"].split(",")[0],
+            )
         self._container.add_layer("udr", self._pebble_layer, combine=True)
         self._container.replan()
         self.unit.status = ActiveStatus()
+
+    @property
+    def _database_relation_is_created(self) -> bool:
+        return self._relation_created("database")
+
+    @property
+    def _database_is_available(self) -> bool:
+        """Returns whether the database is available.
+
+        Returns:
+            bool: Whether the database is available.
+        """
+        return self._database.is_resource_created()
+
+    @property
+    def _database_data(self) -> Dict:
+        """Returns the database data.
+
+        Returns:
+            Dict: The database data.
+        """
+        if not self._database_is_available:
+            raise RuntimeError("Database is not available")
+        return self._database.fetch_relation_data()[self._database.relations[0].id]
 
     @property
     def _nrf_relation_is_created(self) -> bool:
